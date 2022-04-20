4322b67ed0495e520b5dae1d8c08cc26de3003ad
annaalopez <65084157+annaalopez@users.noreply.github.com>
2022-04-20 12:53:55 +0100

    Add files via upload

---
+++ Scripts/csv_format_sec.R
@@ -0,0 +1,15 @@
+INFILES <- ("/home/alopezcarrasco/re_gecip/enhanced_interpretation/AnnaLopez/VEP/sec/")
+OUTFILES <- ("/home/alopezcarrasco/re_gecip/enhanced_interpretation/AnnaLopez/VEP/sec/csv_format/")
+
+file_name <- list.files(INFILES, pattern = ".txt")
+
+read_files <- paste(INFILES, file_name, sep="") 
+write_files <- paste(OUTFILES, paste(sub(".txt","", file_name),".csv"), sep="")
+
+for (i in 1:length(read_files)) {
+  csv_file <- (read.csv(file = read_files[i], header = TRUE, fill = FALSE, sep =""))
+  write.csv(csv_file, file = write_files[i])
+}
+
+
+
